b4564b3d645751fcd0e8142c9e0a763887820896
Stefan Platon <platonfanica@gmail.com>
2019-06-01 18:40:27 +0300

    Removed unecessary comment

--- src/main/java/ecoin/Main.java
+++ src/main/java/ecoin/Main.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//import view.LoginMenu;
-
 /**
  * Starting point of the program.
  */
